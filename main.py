54c50026dea53f30b07e52756431cda39e13d8da
Valentina Mardones <valentinelovagen@gmail.com>
2026-05-11 12:06:39 -0400

    Primer commit de ev3

---
+++ main.py
@@ -0,0 +1,48 @@
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def inicio():
+    return render_template('index.html')
+
+@app.route('/ejercicio1', methods=['GET', 'POST'])
+def ejercicio1():
+    resultado = None
+    promedio = None
+
+    if request.method == 'POST':
+        nota1 = float(request.form['nota1'])
+        nota2 = float(request.form['nota2'])
+        nota3 = float(request.form['nota3'])
+        asistencia = float(request.form['asistencia'])
+
+        promedio = (nota1 + nota2 + nota3) / 3
+
+        if promedio >= 40 and asistencia >= 75:
+            resultado = "APROBADO"
+        else:
+            resultado = "REPROBADO"
+
+    return render_template('ejercicio1.html', promedio=promedio, resultado=resultado)
+
+@app.route('/ejercicio2', methods=['GET', 'POST'])
+def ejercicio2():
+    nombre_mayor = None
+    cantidad = None
+
+    if request.method == 'POST':
+        nombre1 = request.form['nombre1']
+        nombre2 = request.form['nombre2']
+        nombre3 = request.form['nombre3']
+
+        nombres = [nombre1, nombre2, nombre3]
+        nombre_mayor = max(nombres, key=len)
+        cantidad = len(nombre_mayor)
+
+    return render_template('ejercicio2.html', nombre_mayor=nombre_mayor, cantidad=cantidad)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+()
